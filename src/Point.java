b2277db168f8162a97b2dfedafe62d1fbc366930
yairk55 <yairk55@gmail.com>
2024-05-20 15:29:30 +0300

    Changed name

--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,11 @@
-public class Point{
-    int x;
-    int y;
-    public Point(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
+public class Point {
+    // constructor
+    public Point(double x, double y) { }
+    // distance -- return the distance of this point to the other point
+    public double distance(Point other) { }
+    // equals -- return true is the points are equal, false otherwise
+    public boolean equals(Point other) { }
+    // Return the x and y values of this point
+    public double getX() { }
+    public double getY() { }
 }
