a65fb3d328b4d9a8bafa78696996a3158bb2a5c7
shayZingboim <sszingboim@gmail.com>
2024-05-22 14:09:45 +0300

    123

--- src/Line.java
+++ src/Line.java
@@ -1,22 +1,51 @@
 public class Line {
+    private Point start;
+    private Point end;
+
     // constructors
-    public Line(Point start, Point end) { }
-    public Line(double x1, double y1, double x2, double y2) { }
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Line(double x1, double y1, double x2, double y2) {
+        this.start = new Point(x1, y1);
+        this.end = new Point(x2, y2);
+    }
+
     // Return the length of the line
-    public double length() { }
+    public double length() {
+        return start.distance(end);
+    }
+
     // Returns the middle point of the line
-    public Point middle() { }
+    public Point middle() {
+        return new Point(start.getX() + end.getX() / 2, start.getY() + end.getY() / 2);
+    }
+
     // Returns the start point of the line
-    public Point start() { }
+    public Point start() {
+        return this.start;
+    }
+
     // Returns the end point of the line
-    public Point end() { }
+    public Point end() {
+        return this.end;
+    }
+
     // Returns true if the lines intersect, false otherwise
-    public boolean isIntersecting(Line other) { }
+    public boolean isIntersecting(Line other) {
+    }
+
     // Returns true if this 2 lines intersect with this line, false otherwise
-    public boolean isIntersecting(Line other1, Line other2) { }
-    // Returns the intersection point if the lines intersect,
-// and null otherwise.
-    public Point intersectionWith(Line other) { }
+    public boolean isIntersecting(Line other1, Line other2) {
+    }
+
+    // Returns the intersection point if the lines intersect and null otherwise.
+    public Point intersectionWith(Line other) {
+    }
+
     // equals -- return true is the lines are equal, false otherwise
-    public boolean equals(Line other) { }
+    public boolean equals(Line other) {
+    }
 }
