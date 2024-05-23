03863d16eb9690d08b5e07c235414d5e2adfeed9
shayZingboim <sszingboim@gmail.com>
2024-05-23 14:36:47 +0300

    finish the Line class

--- src/Line.java
+++ src/Line.java
@@ -1,6 +1,8 @@
 public class Line {
     private Point start;
     private Point end;
+    private Point inPoint = null;
+    public static final double EPSILON = 0.000001;
 
     // constructors
     public Line(Point start, Point end) {
@@ -20,7 +22,7 @@ public class Line {
 
     // Returns the middle point of the line
     public Point middle() {
-        return new Point(start.getX() + end.getX() / 2, start.getY() + end.getY() / 2);
+        return new Point((start.getX() + end.getX()) / 2, (start.getY() + end.getY()) / 2);
     }
 
     // Returns the start point of the line
@@ -33,16 +35,43 @@ public class Line {
         return this.end;
     }
 
+    // Returns the intersection point
+    public Point getInPoint() {
+        return this.inPoint;
+    }
+
     // Returns true if the lines intersect, false otherwise
     public boolean isIntersecting(Line other) {
         double slope1, slope2, n1, n2, xIntersection, yIntersection;
-        if (this.end.getX() == this.start.getX() && other.end.getX() == this.start.getX() && this.start.getX() != other.start.getX()) {
+        if (this.end.getX() == this.start.getX()
+                && other.end.getX() == other.start.getX()
+                && this.start.getX() != other.start.getX()) {
             return false;
         }
-        if (this.end.getX() == this.start.getX() && other.end.getX() == this.start.getX()) {
-            if ((Math.min(this.start().getY(), this.end.getY()) > Math.max(other.start().getY(), other.end.getY()))
-                    || (Math.min(other.start().getY(), other.end.getY()) > Math.max(this.start().getY(), this.end.getY()))) {
+        if (this.end.getX() == this.start.getX() && other.end.getX() == other.start.getX()) {
+            if ((Math.min(this.start.getY(), this.end.getY()) > Math.max(other.start.getY(), other.end.getY()))
+                    || (Math.min(other.start.getY(), other.end.getY()) > Math.max(this.start.getY(), this.end.getY()))) {
                 return false;
+            }
+            if (((Math.abs(Math.max(this.start.getY(), this.end.getY()))
+                    - (Math.min(other.start.getY(), other.end.getY()))) <= EPSILON)
+                    && !((Math.abs(Math.max(other.start.getY(), other.end.getY()))
+                    - (Math.min(this.start.getY(), this.end.getY()))) <= EPSILON)) {
+                if (this.start.getY() >= this.end.getY()) {
+                    this.inPoint = new Point(this.start.getX(), this.start.getY());
+                } else {
+                    this.inPoint = new Point(this.end.getX(), this.end.getY());
+                }
+            }
+            if (!((Math.abs(Math.max(this.start.getY(), this.end.getY()))
+                    - (Math.min(other.start.getY(), other.end.getY()))) <= EPSILON)
+                    && ((Math.abs(Math.max(other.start.getY(), other.end.getY()))
+                    - (Math.min(this.start.getY(), this.end.getY()))) <= EPSILON)) {
+                if (other.start.getY() >= other.end.getY()) {
+                    this.inPoint = new Point(other.start.getX(), other.start.getY());
+                } else {
+                    this.inPoint = new Point(other.end.getX(), other.end.getY());
+                }
             }
             return true;
         }
@@ -57,6 +86,7 @@ public class Line {
                     && (yIntersection <= Math.max(other.start.getY(), other.end.getY()))
                     && (yIntersection >= Math.min(this.start.getY(), this.end.getY()))
                     && (yIntersection >= Math.min(other.start.getY(), other.end.getY()))) {
+                this.inPoint = new Point(xIntersection, yIntersection);
                 return true;
             }
             return false;
@@ -72,6 +102,7 @@ public class Line {
                     && (yIntersection <= Math.max(other.start.getY(), other.end.getY()))
                     && (yIntersection >= Math.min(this.start.getY(), this.end.getY()))
                     && (yIntersection >= Math.min(other.start.getY(), other.end.getY()))) {
+                this.inPoint = new Point(xIntersection, yIntersection);
                 return true;
             }
             return false;
@@ -94,18 +125,48 @@ public class Line {
             if (n1 != n2) {
                 return false;
             }
-            return true;
+            if (((this.start.getX() <= Math.max(other.start.getX(), other.end.getX())
+                    && this.start.getX() >= Math.min(other.start.getX(), other.end.getX()))
+                    || (this.end.getX() <= Math.max(other.start.getX(), other.end.getX())
+                    && this.end.getX() >= Math.min(other.start.getX(), other.end.getX())))
+                    || (((other.start.getX() <= Math.max(this.start.getX(), this.end.getX()))
+                    && other.start.getX() >= Math.min(this.start.getX(), this.end.getX()))
+                    || (other.end.getX() <= Math.max(this.start.getX(), this.end.getX())
+                    && other.end.getX() >= Math.min(this.start.getX(), this.end.getX())))) {
+                if (((Math.abs(Math.max(this.start.getX(), this.end.getX()))
+                        - (Math.min(other.start.getX(), other.end.getX()))) <= EPSILON)
+                        && !((Math.abs(Math.max(other.start.getX(), other.end.getX()))
+                        - (Math.min(this.start.getX(), this.end.getX()))) <= EPSILON)) {
+                    if (this.start.getX() >= this.end.getX()) {
+                        this.inPoint = new Point(this.start.getX(), this.start.getY());
+                    } else {
+                        this.inPoint = new Point(this.end.getX(), this.end.getY());
+                    }
+                }
+                if (!((Math.abs(Math.max(this.start.getX(), this.end.getX()))
+                        - (Math.min(other.start.getX(), other.end.getX()))) <= EPSILON)
+                        && ((Math.abs(Math.max(other.start.getX(), other.end.getX()))
+                        - (Math.min(this.start.getX(), this.end.getX()))) <= EPSILON)) {
+                    if (other.start.getX() >= other.end.getX()) {
+                        this.inPoint = new Point(other.start.getX(), other.start.getY());
+                    } else {
+                        this.inPoint = new Point(other.end.getX(), other.end.getY());
+                    }
+                }
+                return true;
+            }
         }
-        double x = (n2 - n1) / (slope1 - slope2);
-        double y = x * slope1 + n1;
-        if (x <= Math.max(this.start.getX(), this.end.getX())
-                && (x <= Math.max(other.start.getX(), other.end.getX()))
-                && (x >= Math.min(this.start.getX(), this.end.getX()))
-                && (x >= Math.min(other.start.getX(), other.end.getX()))
-                && (y <= Math.max(this.start.getY(), this.end.getY()))
-                && (y <= Math.max(other.start.getY(), other.end.getY()))
-                && (y >= Math.min(this.start.getY(), this.end.getY()))
-                && (y >= Math.min(other.start.getY(), other.end.getY()))) {
+        xIntersection = (n2 - n1) / (slope1 - slope2);
+        yIntersection = xIntersection * slope1 + n1;
+        if (xIntersection <= Math.max(this.start.getX(), this.end.getX())
+                && (xIntersection <= Math.max(other.start.getX(), other.end.getX()))
+                && (xIntersection >= Math.min(this.start.getX(), this.end.getX()))
+                && (xIntersection >= Math.min(other.start.getX(), other.end.getX()))
+                && (yIntersection <= Math.max(this.start.getY(), this.end.getY()))
+                && (yIntersection <= Math.max(other.start.getY(), other.end.getY()))
+                && (yIntersection >= Math.min(this.start.getY(), this.end.getY()))
+                && (yIntersection >= Math.min(other.start.getY(), other.end.getY()))) {
+            this.inPoint = new Point(xIntersection, yIntersection);
             return true;
         }
         return false;
@@ -121,9 +182,17 @@ public class Line {
 
     // Returns the intersection point if the lines intersect and null otherwise.
     public Point intersectionWith(Line other) {
+        if (this.isIntersecting(other)) {
+            Point intersection = new Point(this.getInPoint().getX(), this.getInPoint().getY());
+            this.inPoint = null;
+            return intersection;
+        }
+        return null;
     }
 
     // equals -- return true is the lines are equal, false otherwise
     public boolean equals(Line other) {
+        return ((this.start.equals(other.start) && this.end.equals(other.end))
+                || (this.start.equals(other.end) && this.end.equals(other.start)));
     }
 }
